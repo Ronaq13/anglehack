205f4d483e1016d65fe7d70bbf2bd11e560aea14
Ronaq13 <raounaqsharma@gmail.com>
2017-05-07 02:49:07 +0530

    doing UI now

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,3 @@
-
-
-
 class MainController < ApplicationController
   def index
   end
@@ -19,7 +16,7 @@ class MainController < ApplicationController
     File.open(Rails.root.join('public', 'uploads','users','books', book_name), 'wb') do |file|
       file.write(actual_book.read)
     end
-    byebug
+
     # Writing data in database
     display_name = params[:display_name]
 
@@ -28,8 +25,19 @@ class MainController < ApplicationController
     book.book_name = book_name
     book.user_id = current_user.id
     book.save
-
+    #generating mp3
+    generatingMP3(book_name,display_name)
     redirect_to '/'
+  end
+
+
+
+  def generatingMP3 (x,display_name)
+
+    y = Yomu.new("#{Rails.root}/public/uploads/users/books/" + x).text
+
+    musicFileName = current_user.id.to_s + "_" + display_name + "." + "mp3"
+    y.to_file "en", "#{Rails.root}/public/uploads/users/audios/" + musicFileName
   end
 
 
@@ -42,6 +50,18 @@ class MainController < ApplicationController
         type: "application/pdf"
     )
   end
+
+  def download_mp3
+    x = params[:id].to_s + "_" + params[:display_name] + ".mp3"
+
+    send_file(
+        "#{Rails.root}/public/uploads/users/audios/" + x,
+        filename: x,
+        type: "mp3"
+    )
+  end
+
+
 
 
 end
